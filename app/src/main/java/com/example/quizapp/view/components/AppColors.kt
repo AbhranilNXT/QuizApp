3d34ba963ae2525beb5b63fbc120ec9ec5d06a28
Abhranil Dasgupta <abhranilnxt@gmail.com>
2024-01-30 12:17:47 +0530

    Modified

---
+++ app/src/main/java/com/example/quizapp/view/components/AppColors.kt
@@ -0,0 +1,19 @@
+package com.example.quizapp.view.components
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    //Custom added colors
+    val _LightGrey = Color(0xFFadb5bd)
+    val _Grey = Color(0xFF6c757d)
+    val _Black = Color(0xFF0a0908)
+    val _OffWhite = Color(0xFFfbf7f5)
+    val _OffWhite2 = Color(0xFFfffff4)
+    val _LightPurple = Color(0xFFb298dc)
+    val _DarkPurple = Color(0xFF402246)
+    val _OffDarkPurple = Color(0xFFa663cc)
+    val _LightBlue = Color(0xFF00b4d8)
+    val _DarkBlue = Color(0xFF0077b6)
+    val _Brown = Color(0xFF432818)
+    val _Blue = Color(0xFF3a0ca3)
+}
--- app/src/main/java/com/example/quizapp/view/components/Questions.kt
+++ app/src/main/java/com/example/quizapp/view/components/Questions.kt
@@ -1,9 +1,27 @@
 package com.example.quizapp.view.components
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quizapp.data.local.UiState
 import com.example.quizapp.vm.QuestionsViewModel
 
@@ -39,4 +57,43 @@ fun Questions(viewModel: QuestionsViewModel) {
         }
 
     }
+}
+
+@Preview
+@Composable
+fun QuestionDisplay() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(8.dp),
+        color = AppColors._DarkPurple) {
+        Column(modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start) {
+
+            QuestionTracker()
+        }
+    }
+}
+
+
+
+@Composable
+fun QuestionTracker( counter : Int = 10, outOf : Int = 100) {
+    Text(text = buildAnnotatedString {
+        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)){
+
+            withStyle(style = SpanStyle(color = AppColors._LightGrey,
+                fontWeight = FontWeight.Bold,
+                fontSize = 26.sp)){
+                append("Question $counter/")
+                withStyle(style = SpanStyle(color = AppColors._LightGrey,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 16.sp)) {
+                    append("$outOf")
+                }
+            }
+        }
+    },
+        modifier = Modifier.padding(20.dp))
 }
